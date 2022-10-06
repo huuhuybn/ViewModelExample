2898b00fa78f4eacd4a0352f8e4660b75cc98de1
HuyNguyen <huuhuybn@gmail.com>
2022-10-06 08:47:05 +0700

    code moi

---
+++ app/src/main/java/com/example/viewmodel/BlankFragment.java
@@ -0,0 +1,41 @@
+package com.example.viewmodel;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class BlankFragment extends Fragment {
+
+
+    private MainViewModel mainViewModel;
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mainViewModel = new ViewModelProvider(getActivity()).get(MainViewModel.class);
+
+        TextView tvText = view.findViewById(R.id.tvText);
+
+        mainViewModel.count.observe(getViewLifecycleOwner(),integer -> {
+            tvText.setText(integer.toString());
+        });
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blank, container, false);
+    }
+}
